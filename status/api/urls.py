633a716d6cee83e4287be19c1fe09b2f967db21f
jay <johnjohnsonokah@gmail.com>
2020-03-22 16:33:16 +0100

    Add migrations to repo

---
+++ status/api/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+
+urlpatterns = [
+    url(r'^$', StatusListSearchAPIView.as_view()),
+    url(r'^create/$', StatusCreateAPIView.as_view()),
+    url(r'^(?P<id>.*)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
+    url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
+]
