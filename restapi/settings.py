c0797107c3e36870f628ba1ac26824bb0a026f46
jay <johnjohnsonokah@gmail.com>
2020-03-22 17:11:13 +0100

    Handle uploading images

--- restapi/settings.py
+++ restapi/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-server', 'media-root')
+MEDIA_URL = '/media/'
